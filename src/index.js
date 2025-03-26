b292c40ed317da87db86ded06c8ab603994a31c3
Sethi <manali.a.sethi@accenture.com>
2025-03-26 17:52:21 +0530

    update backend side socket implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+const path = require("path");
+const { Server } = require('socket.io');
 const express = require('express');
 const { createServer } = require('node:http');
 
 const app = express();
 const server = createServer(app);
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
+const io = new Server(server);
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected',socket);
 });
 
 server.listen(3002, () => {
