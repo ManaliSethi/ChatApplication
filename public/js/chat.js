cee5775ca32607ab65860ad458bdd2b6e1b7375b
Sethi <manali.a.sethi@accenture.com>
2025-03-26 19:16:52 +0530

    update share location and things

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,28 +16,28 @@ const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
-// const autoscroll = () => {
-//   // New message element
-//   const $newMessage = $messages.lastElementChild;
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
 
-//   // Height of the new message
-//   const newMessageStyles = getComputedStyle($newMessage);
-//   const newMessageMargin = parseInt(newMessageStyles.marginBottom);
-//   const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
 
-//   // Visible height
-//   const visibleHeight = $messages.offsetHeight;
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
 
-//   // Height of messages container
-//   const containerHeight = $messages.scrollHeight;
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
 
-//   // How far have I scrolled?
-//   const scrollOffset = $messages.scrollTop + visibleHeight;
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
 
-//   if (containerHeight - newMessageHeight <= scrollOffset) {
-//     $messages.scrollTop = $messages.scrollHeight;
-//   }
-// };
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
 
 socket.on("message", message => {
   const html = Mustache.render(messageTemplate, {
